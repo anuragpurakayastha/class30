ffe9e19151a95b628c398cb72fa5c9528a99a1cb
anuragpurakayastha <anuragpurakayastha729@gmail.com>
2020-10-23 16:28:21 +0530

    pig destroyed

--- sketch.js
+++ sketch.js
@@ -74,4 +74,16 @@ function mouseDragged(){
 
 function mouseReleased(){
     slingshot.fly();
+
+
 }
+
+function keyPressed(){
+    if(keyCode===32){
+        slingshot.attach(bird.body);
+    }
+}
+
+
+
+
